42aeb76d0524c1f63d3d3777fa78174261c88533
kuba <22719248@student.uwa.edu.au>
2020-10-14 17:18:31 +0800

    strct

--- final/managefiles.c
+++ final/managefiles.c
@@ -57,3 +57,40 @@ int Tar()
     }
     return 0;
 }
+
+void removeFile(char *finalpath){
+
+}
+
+void copyFile(){
+
+}
+
+int makeTempFile()
+{
+        char template[] = "/tmp/tmpdir.XXXXXX";
+        char *dir_name = mkdtemp(template);
+
+        if(dir_name == NULL)
+        {
+                perror("mkdtemp failed: ");
+                return 0;
+        }
+
+        /* Use it here */
+        printf("%s", dir_name);
+
+
+
+        /* Don't forget to delete the folder afterwards. */
+        if(rmdir(dir_name) == -1)
+        {
+                perror("rmdir failed: ");
+                return 0;
+        }
+
+
+        return 0;
+
+}
+
--- final/processfiles.c
+++ final/processfiles.c
@@ -61,30 +61,3 @@ void processFiles(char *basedir)
     }
 }
 
-int makeTempFile()
-{
-        char template[] = "/tmp/tmpdir.XXXXXX";
-        char *dir_name = mkdtemp(template);
-
-        if(dir_name == NULL)
-        {
-                perror("mkdtemp failed: ");
-                return 0;
-        }
-
-        /* Use it here */
-        printf("%s", dir_name);
-
-
-
-        /* Don't forget to delete the folder afterwards. */
-        if(rmdir(dir_name) == -1)
-        {
-                perror("rmdir failed: ");
-                return 0;
-        }
-
-
-        return 0;
-
-}
