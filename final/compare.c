02e25b1bc4a30bcfc3b040107580a62e5baed826
kuba <22719248@student.uwa.edu.au>
2020-10-14 17:51:44 +0800

    Update compare.c

--- final/compare.c
+++ final/compare.c
@@ -1,9 +1,50 @@
 #include <headers.h>
 
-int compareTimes(temppath, finalpath){
-
+int getSize(const char *file_path)
+{
+    struct stat s;
+    stat(file_path, &s);
+    int size = s.st_size;
+    return size;
+}
+int getTime(const char *file_path)
+{
+    struct stat s;
+    stat(file_path, &s);
+    int time = s.st_mtime;
+    return time;
 }
 
-int compareSize(file1, file2){
+/**
+ * If tempFile is older, 1
+ * If finalFile is older, -1
+ * both the same, 0 
+ **/
+int compareTimes(const char *temp_path, const char *final_path){
+    int tempTime = getTime(temp_path);
+    int finalTime = getTime(final_path);
 
+    if (tempTime < finalTime){
+        return  1;
+    } else if(tempTime > finalTime) {
+        return -1;
+    }
+        return  0;
+}
+
+/**
+ * If tempFile is smaller, 1
+ * If finalFile is smaller, -1
+ * both the same, 0 
+ **/
+int compareSize(char *temp_path, char *final_path){
+    int tempSize = getSize(temp_path);
+    int finalSize = getSize(final_path);
+
+    if (tempSize < finalSize){
+        return  1;
+    } else if(tempSize > finalSize) {
+        return -1;
+    }
+        return  0;
 }
