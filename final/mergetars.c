e8e4aaf62b842962dc1e21a4b8fde809140d4658
kuba <22719248@student.uwa.edu.au>
2020-10-15 14:00:41 +0800

    final

--- final/mergetars.c
+++ final/mergetars.c
@@ -3,15 +3,19 @@
 //  Student number(s):   22716248, 22502051
 
 #include "headers.h"
+//need to add headers later not source code.c
+#include "compare.c"
+#include "processing.c"
+#include "managefiles.c"
 
 int main(int argc, char *argv[]){
     char finalPath[19];
     memcpy(finalPath, makeFinal(), 19);
-    size_t numberOfTars = sizeof(argv)/sizeof(argv[0]);
-    char output_name[] = argv[numberOfTars-1];
+    size_t numberOfTars = argc - 2;
+    char output_name[] = argv[argc];
 
     int currentTar = 0;
-    while(currentTar != numberOfTars - 2){
+    while(currentTar != numberOfTars - 1){
         char tempPath[19];
         memcpy(tempPath, makeTempFile(), 19);
         unTar(argv[currentTar], tempPath); //untar into temppath
