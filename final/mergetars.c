a2663e0b16e1ee153ec675ee08683741f1ec6be0
kuba <22719248@student.uwa.edu.au>
2020-10-15 15:43:53 +0800

    ok

--- final/mergetars.c
+++ final/mergetars.c
@@ -1,11 +1,8 @@
-#include "headers.h"
-
 //  CITS2002 Project 2 2020
 //  Name(s):             Jakub Wysocki, Brandon Petkovic
 //  Student number(s):   22716248, 22502051
 
 #include "headers.h"
-//need to add headers later not source code.c
 #include "processing.h"
 #include "managefiles.h"
 
